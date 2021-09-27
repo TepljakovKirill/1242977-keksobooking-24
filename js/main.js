c96a3d72f52625333e8807ec7fa7bf0cb345a83f
Kirill Tepljakov <super.css2017@yandex.ru>
2021-09-27 08:42:52 +0300

    поправили ошибки форматирования

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ function randomInteger(min, max) {
   if(max < min || max === min) {
     alert('Это неправильный интервал!');
   }
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
 
-alert( randomInteger(0, 4) );
+randomInteger(0, 4);
 
 function randomIntegerFixed(min, max, toFixed) {
   // получить случайное число от (min-0.5) до (max+0.5)
   if(max < min || max === min) {
     alert('Это неправильный интервал!');
   }
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand).toFixed(toFixed);
 }
 
-alert( randomIntegerFixed(1, 4, 3) );
+randomIntegerFixed(1, 4, 3);
