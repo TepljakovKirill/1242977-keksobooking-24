c41feebe40b0ca1f8ca6df6e46bcc5298568454f
Kirill Tepljakov <super.css2017@yandex.ru>
2021-10-20 19:24:22 +0300

    домашка 7.2

--- js/forma.js
+++ js/forma.js
@@ -3,6 +3,19 @@ const element = document.querySelectorAll('.ad-form__element');
 const filters = document.querySelector('.map__filters');
 const filter = document.querySelectorAll('.map__filter');
 
+function activeForm() {
+  form.classList.remove('ad-form--disabled');
+  for (const elem of element) {
+    elem.removeAttribute('disabled');
+  }
+  filters.classList.remove('ad-form--disabled');
+  for (const elem of filter) {
+    elem.removeAttribute('disabled');
+  }
+}
+
+activeForm();
+
 function disabledForm() {
   form.classList.add('ad-form--disabled');
   for (const elem of element) {
@@ -15,5 +28,3 @@ function disabledForm() {
 }
 
 disabledForm();
-
-
