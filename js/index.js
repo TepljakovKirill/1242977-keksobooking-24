2086f4dd74b4cfc08f32820de58499a0283b9543
Kirill Tepljakov <super.css2017@yandex.ru>
2021-11-09 09:10:57 +0300

    11 домашка

--- js/index.js
+++ js/index.js
@@ -5,4 +5,10 @@ import './popup.js';
 import './forma.js';
 import './filter.js';
 import './leaflet.js';
+import './load.js';
 
+import {createLoader} from './load.js';
+
+const loadAnimals = createLoader(console.log, console.error);
+
+loadAnimals();
